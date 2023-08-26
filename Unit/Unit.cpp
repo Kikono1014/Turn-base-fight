da3843a70f962287d1e0ff2be564ad15392a3c79
Vitalii Pyvovarchuk <vitalik.pyvovarchuk@gmail.com>
2023-08-26 11:52:26 +0300

    add stats

--- Unit/Unit.cpp
+++ Unit/Unit.cpp
@@ -1,8 +1,54 @@
 #include "Unit.h"
 
-Unit::Unit ()
+Unit::Unit (
+        string name, string type,
+        int LVL, int XP,
+        int HP, int MP,
+        int MaxHP, int MaxMP,
+        int ATK, int DEF, int INT, int RES, int DEX 
+    )
 {
+    this->LVL   = LVL  ;
+    this->XP    = XP   ;
+    this->HP    = HP   ;
+    this->MP    = MP   ;
+    this->MaxHP = MaxHP;
+    this->MaxMP = MaxMP;
+    this->ATK   = ATK  ;
+    this->DEF   = DEF  ;
+    this->INT   = INT  ;
+    this->RES   = RES  ;
+    this->DEX   = DEX  ;
+}
 
+int Unit::getLVL   () { return LVL   ; }
+int Unit::getXP    () { return XP    ; }
+int Unit::getHP    () { return HP    ; }
+int Unit::getMP    () { return MP    ; }
+int Unit::getMaxHP () { return MaxHP ; }
+int Unit::getMaxMP () { return MaxMP ; }
+int Unit::getATK   () { return ATK   ; }
+int Unit::getDEF   () { return DEF   ; }
+int Unit::getINT   () { return INT   ; }
+int Unit::getRES   () { return RES   ; }
+int Unit::getDEX   () { return DEX   ; }
+
+void Unit::setLVL   (int LVL  ) { this->LVL   = LVL  ; }
+void Unit::setXP    (int XP   ) { this->XP    = XP   ; }
+void Unit::setHP    (int HP   ) { this->HP    = HP   ; }
+void Unit::setMP    (int MP   ) { this->MP    = MP   ; }
+void Unit::setMaxHP (int MaxHP) { this->MaxHP = MaxHP; }
+void Unit::setMaxMP (int MaxMP) { this->MaxMP = MaxMP; }
+void Unit::setATK   (int ATK  ) { this->ATK   = ATK  ; }
+void Unit::setDEF   (int DEF  ) { this->DEF   = DEF  ; }
+void Unit::setINT   (int INT  ) { this->INT   = INT  ; }
+void Unit::setRES   (int RES  ) { this->RES   = RES  ; }
+void Unit::setDEX   (int DEX  ) { this->DEX   = DEX  ; }
+
+
+void die ()
+{
+    
 }
 
 Unit::~Unit ()
--- Unit/Unit.h
+++ Unit/Unit.h
@@ -1,12 +1,66 @@
 #ifndef UNIT_H
 #define UNIT_H
 
+#include <iostream>
+
+using std::string;
+
 class Unit
 {
 private:
+    string name {};
+    string type {};
+
+    int LVL { 0 };
+
+    int XP { 0 };
+    int HP { 0 };
+    int MP { 0 };
+
+    int MaxHP { 0 };
+    int MaxMP { 0 };
+
+    int ATK { 0 }; // attack
+    int DEF { 0 }; // defense
+    int INT { 0 }; // intelligence
+    int RES { 0 }; // resistance
+    int DEX { 0 }; // dexterity
 
 public:
-    Unit ();
+    Unit (
+        string name, string type,
+        int LVL, int XP,
+        int HP, int MP,
+        int MaxHP, int MaxMP,
+        int ATK, int DEF, int INT, int RES, int DEX 
+    );
+
+    int getLVL   ();
+    int getXP    ();
+    int getHP    ();
+    int getMP    ();
+    int getMaxHP ();
+    int getMaxMP ();
+    int getATK   ();
+    int getDEF   ();
+    int getINT   ();
+    int getRES   ();
+    int getDEX   ();
+
+    void setLVL   (int LVL  );
+    void setXP    (int XP   );
+    void setHP    (int HP   );
+    void setMP    (int MP   );
+    void setMaxHP (int MaxHP);
+    void setMaxMP (int MaxMP);
+    void setATK   (int ATK  );
+    void setDEF   (int DEF  );
+    void setINT   (int INT  );
+    void setRES   (int RES  );
+    void setDEX   (int DEX  );
+
+    void die ();
+
     ~Unit ();
 
 };
