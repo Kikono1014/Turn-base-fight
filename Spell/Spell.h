a82c92f1ce3ca783aef632fc84ea99c205e02436
Vitalii Pyvovarchuk <vitalik.pyvovarchuk@gmail.com>
2023-09-17 06:12:57 +0300

    add base things to spell

--- Spell/Spell.h
+++ Spell/Spell.h
@@ -1,12 +1,18 @@
 #ifndef SPELL_H
 #define SPELL_H
+#include <iostream>
+
+using std::string;
 
 class Spell
 {
 private:
-
+    string type {};
 public:
     Spell ();
+
+    void use ();
+
     ~Spell ();
 
 };
